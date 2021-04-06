ae796db590b4d04aed79fe0517b9b61694160a24
Colin Tseng <klt.aus@yahoo.com>
2021-04-06 21:28:55 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your Circle class here
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+
+    }
+    // get diamter is a pseudo property and can be access like a regular property via dot notation or bracket notation
+    get diameter()  {
+        return this.radius * 2
+    }
+    get circumference() {
+        return 2 * (Math.PI) * this.radius
+    }
+    get area() {
+        return (Math.PI) * (this.radius * this.radius)
+    }
+    set diameter(a) {
+        this.radius = a / 2;
+    }
+    set circumference(a) {
+        this.radius = (a / 2 / (Math.PI));
+    }
+}
